f9bcc43f360d4b8b1f781d93a1326f52fe91402c
Kasra Naderi <naderi.kasra@gmail.com>
2021-01-29 12:37:45 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import web
+
+
+urls = (
+    '/', 'index'
+)
+
+app = web.application(urls, globals())
+
+
+class index:
+    def GET(self):
+        return "Hello World"
+
+
+if __name__ == '__main__':
+    app.run()
